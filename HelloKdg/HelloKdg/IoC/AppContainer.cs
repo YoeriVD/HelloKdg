c8ef3d0de9a9b8d97554d4c6df1ab98e4d567b81
YoeriVD <yoeri.vandamme@gmail.com>
2016-02-10 19:35:30 +0100

    add autofac

---
+++ HelloKdg/HelloKdg/IoC/AppContainer.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Autofac;
+
+namespace HelloKdg.IoC
+{
+    internal class AppContainer
+    {
+        public IContainer CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+            // Registers all modules
+            builder.RegisterAssemblyModules(GetType().GetTypeInfo().Assembly);
+            return builder.Build();
+        }
+    }
+}
---
+++ HelloKdg/HelloKdg/Main/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloKdg.Main
+{
+    internal interface IMainViewModel
+    {
+    }
+
+    class MainViewModel : IMainViewModel
+    {
+    }
+}
